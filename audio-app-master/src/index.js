575bc1b2c9a910073870ad501343181b4e83a12a
rscheiwe <rscheiwe@gmail.com>
2018-08-20 14:21:34 -0400

    add libraries

--- audio-app-master/src/index.js
+++ audio-app-master/src/index.js
@@ -4,7 +4,7 @@ import './App.css';
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
-import App from './App';
+import App from './Home';
 
 
 ReactDOM.render(<App />, document.getElementById('app'));
